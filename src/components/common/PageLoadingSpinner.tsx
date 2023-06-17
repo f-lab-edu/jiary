822ea4792a6c804a6462def38c0e40ee9fe2da1b
pozafly <pozafly@gmail.com>
2023-06-17 18:10:20 +0900

    feat: add whole page loading component

---
+++ src/components/common/PageLoadingSpinner.tsx
@@ -0,0 +1,13 @@
+import * as style from '@/components/common/PageLoadingSpinner.css.ts';
+
+export default function LoadingSpinner() {
+  return (
+    <div className={style.loadingSpinnerContainer}>
+      <div className={style.loadingSpinner}>
+        <div className={style.div1}></div>
+        <div className={style.div2}></div>
+        <div className={style.div3}></div>
+      </div>
+    </div>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,8 +2,15 @@ import Head from 'next/head';
 import Counter from '../components/Counter.tsx';
 import * as style from '../styles/style-test.css.ts';
 import QueryTest from '../components/QueryTest.tsx';
+import PageLoadingSpinner from '@/components/common/PageLoadingSpinner.tsx';
+import { ReducerType } from '@/store/rootReducer.ts';
+import { useSelector } from 'react-redux';
 
 export default function Home() {
+  const isPageLoading = useSelector(
+    (state: ReducerType) => state.ui.isPageLoading
+  );
+
   return (
     <>
       <Head>
@@ -14,6 +21,8 @@ export default function Home() {
         <Counter />
         <br />
         <QueryTest />
+        <br />
+        {isPageLoading ? <PageLoadingSpinner /> : null}
       </main>
     </>
   );
