d1c7db89e2fdac0c5b3a33c5061da4d7853717f4
pozafly <pozafly@gmail.com>
2023-06-28 18:32:48 +0900

    refactor: page layout css 수정

--- src/features/common/components/PageLayout.css.ts
+++ src/features/common/components/PageLayout.css.ts
@@ -2,5 +2,7 @@ import { style } from '@vanilla-extract/css';
 
 export const main = style({
   paddingTop: '56px',
-  backgroundColor: '#1a1b1e',
+  backgroundColor: '#eeeff2',
+  minHeight: 'calc(100vh - 56px)',
+  margin: '0 auto',
 });
