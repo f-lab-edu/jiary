20fd8e9d14ff88a0e46b5d4088e3a5e3079aacb7
pozafly <pozafly@gmail.com>
2023-06-30 18:45:59 +0900

    fix: 다이어리 카드 제목 jiary- 제거하여 보여주도록 수정

--- src/features/diary/components/diaryList/DiaryCard.tsx
+++ src/features/diary/components/diaryList/DiaryCard.tsx
@@ -7,13 +7,15 @@ import earth from '@/static/diary/earth2.svg';
 import calendar from '@/static/diary/calendar.svg';
 
 export default function DiaryCard({ file }: { file: File }) {
+  const title = file.name.includes('jiary-') ? file.name.slice(6) : file.name;
+
   return (
     <li key={file.id} className={style.li}>
       <Link href={`diary/${file.id}`} className={style.link}>
         <div className={style.header}>
           <div className={style.location}>
             <Image src={earth} width={20} height={20} alt="earth icon" />
-            {/* location */}
+            {/* TODO: 하드 코딩 remove */}
             <span className={style.locationText}>Unitied State</span>
           </div>
           <button>
@@ -21,11 +23,12 @@ export default function DiaryCard({ file }: { file: File }) {
           </button>
         </div>
         <div className={style.contentWrapper}>
-          <span className={style.title}>{file.name}</span>
-          <div className={style.tag}></div>
+          <span className={style.title}>{title}</span>
+          <div className={style.tag}>{/* TODO: 하드 코딩 remove */}</div>
         </div>
         <div className={style.footer}>
           <Image src={calendar} width={20} height={20} alt="calendar icon" />
+          {/* TODO: 하드 코딩 remove */}
           <span>May 12, 2020</span>
         </div>
       </Link>
