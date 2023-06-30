1ea689c395f89aba494fd50a0aa847b901fce90a
pozafly <pozafly@gmail.com>
2023-06-30 16:06:20 +0900

    feat: add constant color system

---
+++ src/constant/colors.ts
@@ -0,0 +1,17 @@
+export const COLORS = {
+  BORDER_COLOR: 'hsl(230, 25%, 94%)',
+  SECOND_TEXT_COLOR: 'hsl(230, 10%, 65%)',
+  TAG_BACKGROUND: 'hsl(0, 0%, 85%)',
+
+  BLUE_COLOR: 'hsl(226, 100%, 62%)',
+  BLUE_LIGHT_COLOR: 'hsl(226, 100%, 65%);',
+
+  GREEN_COLOR: 'hsl(157, 64%, 41%)',
+  GREEN_LIGHT_COLOR: 'hsl(155, 64%, 46%)',
+
+  RED_COLOR: 'hsl(11, 76%, 54%)',
+  RED_LIGHT_COLOR: 'hsl(11, 76%, 59%)',
+
+  ORANGE_COLOR: 'hsl(32, 100%, 50%)',
+  ORANGE_LIGHT_COLOR: 'hsl(32, 100%, 55%)',
+} as const;
