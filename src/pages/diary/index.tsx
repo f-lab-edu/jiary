cb2447f56e661237cd24437f6679487a7d75fda8
pozafly <pozafly@gmail.com>
2023-06-30 19:31:26 +0900

    feat: 다이어리 리스트 페이지 로딩 스피너 추가

--- src/pages/diary/index.tsx
+++ src/pages/diary/index.tsx
@@ -1,12 +1,17 @@
 import { useGetDocList } from '@/features/diary/apis/queries.ts';
 import DiaryList from '@/features/diary/components/diaryList/DiaryList.tsx';
+import { useIsFetching } from '@tanstack/react-query';
+import { DIARY_KEY } from '@/features/diary/apis/index.ts';
+import PageLoadingSpinner from '@/features/common/components/spinner/PageLoadingSpinner.tsx';
 
 export default function Index() {
   const { data } = useGetDocList();
+  const isPageLoading = useIsFetching({ queryKey: [DIARY_KEY] });
 
   return (
     <>
       <DiaryList files={data?.files} />
+      {isPageLoading && <PageLoadingSpinner />}
     </>
   );
 }
