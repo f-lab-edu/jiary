faf208272bbd18b29809a10666059209a0f54698
pozafly <pozafly@gmail.com>
2023-07-07 17:28:39 +0900

    fix: image 도메인host 경로를 와일드 카드 처리

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ const nextConfig = {
   },
   images: {
     remotePatterns: [
-      { protocol: 'https', hostname: 'lh3.googleusercontent.com' },
+      { protocol: 'https', hostname: '*.googleusercontent.com' },
     ],
   },
 };
