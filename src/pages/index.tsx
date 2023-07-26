101b7b26f878edb810904744003074b390a7b824
pozafly <pozafly@gmail.com>
2023-07-26 22:41:07 +0900

    fix: rebase 후 import 구문 살아있는 문제로 import문 제거

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import Head from 'next/head';
-import * as style from '@/styles/style-test.css.ts';
 import { getAccessTokenByRefreshToken } from '@/features/auth/apis/index.ts';
 
 export default function Home() {
@@ -9,7 +8,7 @@ export default function Home() {
         <title>홈 입니다</title>
       </Head>
       <button onClick={getAccessTokenByRefreshToken}>get refresh token!</button>
-      .<div className={style.Main}>Home</div>
+      <div>Home</div>
     </>
   );
 }
