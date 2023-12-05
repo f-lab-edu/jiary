696cf47d68e9340b07088484808826ba05ecab55
pozafly <pozafly@gmail.com>
2023-12-05 17:16:54 +0900

    fix: 로그인 팝업 모달에서 CORS 오류 해결
    - next.config.js에 CORS 관련 헤더 추가

--- next.config.js
+++ next.config.js
@@ -12,6 +12,24 @@ const nextConfig = {
       { protocol: 'https', hostname: '*.googleusercontent.com' },
     ],
   },
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: process.env.NEXT_PUBLIC_DOMAIN,
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+          },
+        ],
+      },
+    ];
+  },
   webpack: config => {
     config.module.rules.push({
       test: /\.svg$/,
