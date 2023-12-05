810b44bce88329807010435a01ee78209c0ed016
pozafly <pozafly@gmail.com>
2023-12-05 23:15:15 +0900

    refactor: middleware 복구

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,9 +7,8 @@ export const config = {
 
 export function middleware(request: NextRequest) {
   const authCookie = request.cookies.get('Authorization');
-  console.log('authCookie', authCookie);
-  // if (!authCookie) {
-  //   return NextResponse.redirect(new URL('/auth', request.url));
-  // }
+  if (!authCookie) {
+    return NextResponse.redirect(new URL('/auth', request.url));
+  }
   return NextResponse.next();
 }
