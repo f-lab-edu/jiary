5d90a410e3ee926a177b355c2c1fca3cc312b43f
pozafly <pozafly@gmail.com>
2023-12-06 17:16:03 +0900

    fix: login url 원상 복구 및 SSR 적용

--- src/constants/domain.ts
+++ src/constants/domain.ts
@@ -3,4 +3,3 @@ export const OAUTH_DOMAIN = 'https://www.googleapis.com';
 export const DRIVE_DOMAIN = 'https://www.googleapis.com/drive/v3/files';
 export const DRIVE_UPLOAD_DOMAIN =
   'https://www.googleapis.com/upload/drive/v3/files';
-export const GOOGLE_POPUP_URL = `https://accounts.google.com/o/oauth2/v2/auth?access_type=offline&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.profile%20https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email%20openid%20https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fdrive.appdata&response_type=code&client_id=799247539118-dkci5957fbdpf3546piiur8odfutjmf5.apps.googleusercontent.com&redirect_uri=http%3A%2F%2F${JIARY_DOMAIN}%2Fauth%2Fend-popup`;
--- src/pages/diary/index.tsx
+++ src/pages/diary/index.tsx
@@ -1,1 +1,23 @@
-export { default } from '@/features/diary/pages/root/DiaryPage.tsx';
+import { dehydrate, QueryClient } from '@tanstack/react-query';
+import { GetServerSideProps } from 'next';
+
+import { AUTH_KEY } from '@/constants/queryKey.ts';
+import { getAuthCode } from '@/features/auth/apis/index.ts';
+import { GoogleLoginUrl } from '@/features/auth/apis/interfaces.ts';
+
+export { default } from '@/features/auth/pages/root/AuthPage.tsx';
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const queryClient = new QueryClient();
+  await queryClient.prefetchQuery<GoogleLoginUrl>({
+    queryKey: [AUTH_KEY, 'code'],
+    queryFn: getAuthCode,
+    staleTime: Infinity,
+  });
+
+  return {
+    props: {
+      dehydratedState: dehydrate(queryClient),
+    },
+  };
+};
