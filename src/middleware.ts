2abf10b5ca1f1eb806348c4d7ede527653509da6
pozafly <pozafly@gmail.com>
2023-12-06 18:10:55 +0900

    fix: last 배포 테스트

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,14 +2,15 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 // NOTE: 로그인 시 diary redirect 이슈로 인한 주석처리
-// export const config = {
-//   matcher: ['/diary/:path*'],
-// };
+export const config = {
+  matcher: ['/diary/:path*'],
+};
 
 export function middleware(request: NextRequest) {
-  const authCookie = request.cookies.get('Authorization');
-  if (!authCookie) {
-    // return NextResponse.redirect(new URL('/auth', request.url));
+  // const authCookie = request.cookies.get('Authorization');
+  if (!request.cookies.has('Authorization')) {
+    console.log('리다이렉트!');
+    return NextResponse.redirect(new URL('/auth', request.url));
   }
 
   return NextResponse.next();
