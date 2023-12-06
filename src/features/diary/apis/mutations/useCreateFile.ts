5fc7707c64d303cf05d1e62944b07c24039008e7
pozafly <pozafly@gmail.com>
2023-12-06 20:19:00 +0900

    feat: 카드 생성 후 기존 list data 무효화 설정

--- src/features/diary/apis/mutations/useCreateFile.ts
+++ src/features/diary/apis/mutations/useCreateFile.ts
@@ -1,12 +1,18 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 
 import { DIARY_KEY } from '@/constants/queryKey.ts';
 import { createFile } from '@/features/diary/apis/index.ts';
+import { DriveFile } from '@/features/diary/apis/interfaces.ts';
 
-const useCreateFile = () =>
-  useMutation({
+const useCreateFile = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
     mutationKey: [DIARY_KEY],
     mutationFn: (title: string) => createFile(title),
+    onSuccess() {
+      queryClient.invalidateQueries<DriveFile>([DIARY_KEY, 'docList']);
+    },
   });
+};
 
 export default useCreateFile;
