08b445a2f58d7d49f396699804c1d675ed4b33d2
pozafly <pozafly@gmail.com>
2023-12-06 17:37:04 +0900

    fix: route 이동 버그 수정
    - diary page 소스 코드가 엉키면서, auth를 바라봄
    - middleware에 startwith 코드 제거

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,13 +2,13 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 // NOTE: 로그인 시 diary redirect 이슈로 인한 주석처리
-// export const config = {
-//   matcher: ['/diary/:path*'],
-// };
+export const config = {
+  matcher: ['/diary/:path*'],
+};
 
 export function middleware(request: NextRequest) {
   const authCookie = request.cookies.get('Authorization');
-  if (request.nextUrl.pathname.startsWith('/diary/*') && !authCookie) {
+  if (!authCookie) {
     return NextResponse.redirect(new URL('/auth', request.url));
   }
 
